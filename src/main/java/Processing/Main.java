705acec2c3da0277cdf8cfe8a1ba78f1cf759a6e
diogolopes18-cyber <diogolopes18@ieee.org>
2022-10-22 22:22:19 +0100

    feat: define main class for creating a new payment

---
+++ src/main/java/Processing/Main.java
@@ -0,0 +1,38 @@
+package Processing;
+
+import Model.PaymentInfo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    private static void SetNewPayment(){
+        PaymentInfo payment = new PaymentInfo();
+        payment.setPaymentId(1);
+        payment.setAmount(40);
+        payment.setDestinationaccount("PT50055515");
+        payment.setPaymentcomment("test");
+    }
+
+    public static void Main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("PaymentInfo");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try{
+            transaction.begin();
+            SetNewPayment();
+            transaction.commit();
+        } finally {
+            if(transaction.isActive()){
+                transaction.rollback();
+            }
+
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+}
